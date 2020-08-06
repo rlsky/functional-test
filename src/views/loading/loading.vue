1416fa923a81c69f38ea72df18bbfc92598bb449
renlei1 <renlei1@ithro.com.cn>
2020-08-06 17:20:15 +0800

    fix:更新

---
+++ src/views/loading/loading.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="loading">
+    <div class="wrap"></div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+<style></style>
--- src/views/ly-Tab/index.vue
+++ src/views/ly-Tab/index.vue
@@ -1,21 +1,15 @@
 <template>
   <div id="ly-tab-wrap">
-    <ly-tab
-      v-model="selectedId"
-      :items="items"
-      :options="options"
-      :change="selectChange()"
-    >
-    </ly-tab>
     <div class="js-Vue-Tab-Wrap">
       <div class="js-Vue-Tab">
-        <ul class="tab-List" @cllick="listclick" ref="tabList">
+        <ul class="tab-List" ref="tabList">
           <li
             class="tab-Item"
             @click="itemTouch($event, item.id)"
             v-for="item in jsItems"
             :key="item.id"
             :class="{ activeItem: item.id == isActive }"
+            ref="tabItem"
           >
             {{ item.txt }}
           </li>
@@ -23,32 +17,67 @@
         </ul>
       </div>
     </div>
+    <ly-tab
+      v-model="selectedId"
+      :items="items"
+      :options="options"
+      :change="selectChange()"
+    >
+    </ly-tab>
   </div>
 </template>
 <script>
 export default {
+  mounted() {
+    this.isFirstIn();
+  },
   methods: {
+    // 首次进入页面,tabline的位置定位
+    isFirstIn() {
+      this.$refs.tabLine.style.width = `22.8px`;
+      let FirstTargetW = this.$refs.tabItem[0].offsetWidth / 2;
+      let X = FirstTargetW - 11 + "px";
+      this.$refs.tabLine.style.transform = `translateX( ${X} )`;
+    },
     selectChange() {
       console.log(this.selectedId);
     },
-    listclick(e) {
-      console.log(e, "111");
-    },
+    // 每次点击tab,根据点击的tabitem移动tab
     itemTouch(e, id) {
+      // 点击左侧时
+      if (
+        this.RelativeParentLeft > e.target.offsetLeft &&
+        e.target.getBoundingClientRect().left < 150
+      ) {
+        this.$refs.tabList.scrollTo({
+          left: e.target.offsetLeft - 160,
+          behavior: "smooth",
+        });
+      } else {
+        // 点击右侧时
+        if (
+          e.target.getBoundingClientRect().left + e.target.offsetWidth >
+          200
+        ) {
+          this.$refs.tabList.scrollTo({
+            left: e.target.offsetLeft - 155,
+            behavior: "smooth",
+          });
+        }
+      }
+      this.RelativeParentLeft = e.target.offsetLeft;
+
       this.isActive = id;
       let targetW = e.target.offsetWidth / 2;
       let LineW = this.$refs.tabLine.offsetWidth / 2;
-      let X = e.target.offsetLeft + targetW - LineW + "px";
-      if (e.target.offsetLeft > 220) {
-        console.log("大于220了");
-        this.$refs.tabList.scrollLeft = 100;
-      }
+      let X = this.RelativeParentLeft + targetW - LineW + "px";
       this.$refs.tabLine.style.transform = `translateX( ${X} )`;
     },
   },
   // ly-tab插件git地址 https://github.com/ScoutYin/ly-tab
   data() {
     return {
+      RelativeParentLeft: 0,
       isActive: 0,
       selectedId: 0,
       items: [
@@ -75,6 +104,7 @@ export default {
         { txt: "产品", id: 6 },
         { txt: "人工智能", id: 7 },
         { txt: "设计", id: 8 },
+        { txt: "设计1", id: 9 },
       ],
     };
   },
@@ -86,12 +116,15 @@ export default {
   width: 100%;
   .js-Vue-Tab-Wrap {
     height: 48px;
-    width: 300px;
+    width: 100%;
     display: inline-block;
-    background: burlywood;
+    margin-bottom: 100px;
     .js-Vue-Tab {
       height: 100%;
       width: 100%;
+      border-bottom: 1px solid #eee;
+      box-shadow: 0 0px 6px 1px #eee;
+      background-color: rgba(255, 255, 255, 0.8);
       .tab-List {
         height: 100%;
         width: 100%;
@@ -104,7 +137,6 @@ export default {
         .tab-Item {
           padding: 0 11px;
           white-space: nowrap;
-          background: bisque;
           font-size: 14px;
           line-height: 48px;
         }
@@ -115,8 +147,8 @@ export default {
           position: absolute;
           left: 0;
           bottom: 3px;
-          width: 22.8px;
-          height: 3px;
+          width: 0px;
+          height: 2px;
           background: rgb(29, 152, 189);
           border-radius: 4px;
           transition: all 0.5s;
