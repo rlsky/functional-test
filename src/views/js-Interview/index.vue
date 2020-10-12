647576916c6e0867a8187d274d2612e3798a573d
renlei1 <renlei1@ithro.com.cn>
2020-10-12 15:41:51 +0800

    fix:更新

--- src/views/js-Interview/index.vue
+++ src/views/js-Interview/index.vue
@@ -1,15 +1,87 @@
 <template>
-  <div>js</div>
+  <div>
+    <div @click="toclick">js</div>
+    <div class="Tremble" @touchmove="move2">
+      函数防抖{{ num }}
+      <div></div>
+    </div>
+    <input @input="enter" type="text" placeholder="函数节流" />
+    <span
+      v-for="(name, i) in couponNameList"
+      v-show="commission ? i < 1 : i < 2"
+      :key="i"
+    >
+      <i>&nbsp;{{ name }}&nbsp;</i>
+    </span>
+  </div>
 </template>
 <script>
 export default {
-  name: "jsInterview",
+  name: 'jsInterview',
   data() {
-    return {};
+    return {
+      urlLink: '',
+      num: 1,
+      timeoutId: null,
+      enterId: false,
+      couponNameList: ['满100减10', '满200减20'],
+      commission: ''
+    }
   },
   created() {},
   mounted() {},
-  methods: {},
-};
+  methods: {
+    toclick() {
+      setTimeout(() => {
+        window.location.href = this.urlLink
+        // alert(window.location.href)
+        window.location.reload()
+      }, 100)
+      this.urlLink = `http://10.7.0.212:3001/#/home`
+      if (this.urlLink.indexOf('?') > 0) {
+        this.urlLink +=
+          '&t=' + new Date().getTime() + 's' + this.getRandom(1000, 9999)
+      } else {
+        this.urlLink +=
+          '?t=' + new Date().getTime() + 's' + this.getRandom(1000, 9999)
+      }
+    },
+    // 获取随机数的函数
+    getRandom(start, end, fixed = 0) {
+      let differ = end - start
+      let random = Math.random()
+      return (start + differ * random).toFixed(fixed)
+    },
+    // 函数防抖
+    move2() {
+      console.log('1111')
+      let that = this
+      this.timeoutId && clearTimeout(this.timeoutId)
+      this.timeoutId = setTimeout(() => {
+        that.num = ++that.num
+      }, 1000)
+    },
+    sendAjax() {
+      console.log('发送ajax请求')
+    },
+    // 函数节流
+    enter() {
+      if (this.enterId) {
+        return
+      }
+      this.enterId = true
+      setTimeout(() => {
+        this.sendAjax()
+        this.enterId = false
+      }, 1000)
+    }
+  }
+}
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.Tremble {
+  height: 400px;
+  width: 100%;
+  background: grey;
+}
+</style>
--- src/views/swiper-View/index.vue
+++ src/views/swiper-View/index.vue
@@ -1,26 +1,17 @@
 <template>
   <div id="swiperView">
-    swiperView
-    <swiper :options="swiperOption">
-      <swiper-slide>
-        <div class="item1">111</div>
-      </swiper-slide>
-      <swiper-slide>
-        <div class="item2">222</div>
-      </swiper-slide>
-      <swiper-slide>
-        <div class="item3">333</div>
-      </swiper-slide>
-      <swiper-slide>
-        <div class="item1">111</div>
-      </swiper-slide>
-      <swiper-slide>
-        <div class="item2">222</div>
-      </swiper-slide>
-      <swiper-slide>
-        <div class="item3">333</div>
-      </swiper-slide>
-    </swiper>
+    <div class="logistics-Tracking">
+      <div class="one-Info" v-for="(item, index) in tracesData" :key="index">
+        <div class="drop" :class="index === 0 ? 'drop-First' : ''"></div>
+        <div class="drop-Wrap" v-show="index === 0"></div>
+        <div class="text-Time">
+          <p class="text">
+            {{ item.context }}
+          </p>
+          <span class="time">{{ item.ftime }}</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -28,21 +19,25 @@ export default {
   name: 'swiperView',
   data() {
     return {
-      swiperOption: {
-        slidesPerView: 2,
-        freeMode: true,
-        direction: 'vertical',
-        navigation: {
-          nextEl: '.next',
-          prevEl: '.prev'
+      tracesData: [
+        {
+          context: '哈哈哈',
+          ftime:
+            '我去我去我去我去我去我去我去我去我去我去我去我去我去我去我去我去我去我去我去我去我去我去我去我去我去我去我去我去'
         },
-        autoplay: {
-          delay: 2000,
-          stopOnLastSlide: false /* 触摸滑动后是否继续轮播 */,
-          disableOnInteraction: false
+        {
+          context: '哈哈哈',
+          ftime: '我去'
         },
-        loop: true
-      }
+        {
+          context: '哈哈哈',
+          ftime: '我去'
+        },
+        {
+          context: '哈哈哈',
+          ftime: '我去'
+        }
+      ]
     }
   },
   created() {},
@@ -50,34 +45,58 @@ export default {
   methods: {}
 }
 </script>
-<style>
-.swiper-container {
-  height: 180px;
-}
-.swiper-slide {
-  height: 90px;
-  /* border-bottom: 10px solid gray; */
-  /* box-sizing: border-box; */
-}
-.item1 {
-  height: 90px;
-  width: 100%;
-  background: red;
-  border-bottom: 1px solid gray;
-  box-sizing: border-box;
-}
-.item2 {
-  height: 90px;
-  width: 100%;
-  background: green;
-  border-bottom: 1px solid gray;
-  box-sizing: border-box;
-}
-.item3 {
-  height: 90px;
-  width: 100%;
-  background: cadetblue;
-  border-bottom: 1px solid gray;
-  box-sizing: border-box;
+<style lang="scss" scoped>
+.logistics-Tracking {
+  padding: 52px 32px 32px 58px;
+  background: rgba(255, 255, 255, 1);
+  .one-Info {
+    position: relative;
+    border-left: 2px solid rgba(225, 226, 233, 1);
+    padding: 0 0 56px 32px;
+    &:last-child {
+      border: none;
+      padding-bottom: 0;
+    }
+    .drop {
+      height: 10px;
+      width: 10px;
+      background: rgba(225, 226, 233, 1);
+      position: absolute;
+      left: -6px;
+      top: 0;
+      border-radius: 50%;
+      z-index: 9;
+    }
+    .drop-First {
+      background: rgba(255, 60, 60, 1);
+    }
+    .drop-Wrap {
+      position: absolute;
+      left: -11px;
+      top: -4.5px;
+      width: 20px;
+      height: 20px;
+      background: rgba(253, 161, 155, 1);
+      opacity: 0.3;
+      border-radius: 50%;
+    }
+    .text-Time {
+      .text {
+        font-size: 28px;
+        font-family: PingFangSC-Regular, PingFang SC;
+        transform: translateY(-18px);
+        color: rgba(51, 51, 51, 1);
+        word-break: break-all;
+        .name-PhoneNum {
+          color: #ff3c3c;
+        }
+      }
+      .time {
+        font-size: 24px;
+        font-family: PingFangSC-Regular, PingFang SC;
+        color: rgba(153, 153, 153, 1);
+      }
+    }
+  }
 }
 </style>
