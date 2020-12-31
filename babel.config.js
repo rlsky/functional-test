b9e717e5fdd58b1cb3d2aacaa9791acd751fe751
renlei1 <renlei1@ithro.com.cn>
2020-12-31 15:10:14 +0800

    fix:引入element-ui和vant

--- babel.config.js
+++ babel.config.js
@@ -10,5 +10,12 @@ module.exports = {
       },
       "vant",
     ],
+    [
+      "component",
+      {
+        libraryName: "element-ui",
+        styleLibraryName: "theme-chalk"
+      }
+    ]
   ],
 };
